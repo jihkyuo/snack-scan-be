6ef3b5370126d998406c48a7a0db640ac45bbcc7
지오현 <jihkyuoo@naver.com>
2025-10-02 22:13:48 +0900

    setting: spring init

---
+++ src/main/java/com/snackscan/SnackScanApplication.java
@@ -0,0 +1,13 @@
+package com.snackscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackScanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snackscan/SnackScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snackscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
